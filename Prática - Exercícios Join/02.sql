5f3e4e7167690f14805f61484f469e16620a103c
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2022-12-14 19:50:43 -0300

    exercicios finalizados

---
+++ Prática - Exercícios Join/02.sql
@@ -0,0 +1,5 @@
+SELECT testimonials.id, users.name AS writer, u.name AS recipient, testimonials.message
+FROM testimonials JOIN users
+ON testimonials."writerId" = users.id
+JOIN users u
+ON testimonials."recipientId" = u.id;
---
+++ Prática - Exercícios Join/04.sql
@@ -0,0 +1,14 @@
+SELECT users.id, users.name, roles.name AS "role", companies.name AS company, experiences."startDate"
+FROM users
+JOIN experiences
+ON users.id = experiences."userId"
+JOIN roles
+ON experiences."roleId" = roles.id
+JOIN companies
+ON experiences."companyId" = companies.id
+WHERE experiences."userId"=50;
+
+-- |---id---|---name---|--------role--------|----company----|---startDate---|
+--     1        João      Software Engineer        OLX          2020-06-01
+--     2        João         Consultant            IBM          2022-02-01
+--    ...        ...            ...                ...              ...
